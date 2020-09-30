2a615850b2efbca63107d4b36582e3f2d4703194
tkkai-1011 <59884172+tkkai-1011@users.noreply.github.com>
2020-09-30 13:06:34 -0400

    Delete configtest.py

--- configtest.py
+++
@@ -1,9 +0,0 @@
-import re
-
-def password_validator(passw):
-    if len(passw) < 9:
-        return False
-    elif not bool(re.search(r'\d', passw)):
-        return False
-    else:
-        return True
